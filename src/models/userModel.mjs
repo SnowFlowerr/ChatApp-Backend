c483c7b57d3d197dae1c45b206b8641c595bc761
Bhudeo Krit <Bhudeokrit@gmail.com>
2024-05-12 13:03:27 +0530

    image added

--- src/models/userModel.mjs
+++ src/models/userModel.mjs
@@ -6,5 +6,6 @@ const userSchema=new mongoose.Schema({
     time:String,
     email:String,
     video:String,
+    img:String,
 },{timestamps:true});
 export default mongoose.model('user',userSchema);
