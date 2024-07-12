9ef5884f93ea9f5d60e95c47f7886b15b1456abc
SnowFlowerr <bhudeokrit@gmail.com>
2024-07-12 12:20:46 +0530

    env file added

--- src/index.mjs
+++ src/index.mjs
@@ -2,12 +2,22 @@ import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
 import router from "./routes/route.mjs";
+import dotenv from 'dotenv'
 
 const app=express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb+srv://bhudeokrit:Uzhizz996x@cluster0.vxgwpky.mongodb.net/ChattingData?retryWrites=true&w=majority").then(()=>{console.log("Database Connected Successfuly")}).catch((err)=>{console.log(err)});
+dotenv.config()
+const connectApp=()=>{
+    mongoose.connect(process.env.MONGODB).then(()=>{
+        console.log("Database Connected")
+    }).catch((error)=>{
+        console.log(error);
+        throw error;
+    })
+}
+connectApp()
 
 app.use('/',router);
 app.listen(8000,()=>{
